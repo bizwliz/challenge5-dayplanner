7bfe1acffc0afe642e48e791d0681294a2624729
bizwliz <glissette1999@gmail.com>
2023-07-20 18:01:19 -0400

    added notes to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// wrapped code inside jQuery read function
 $(function () {
 var currentDayEL=$("#currentDay")
 var currentDayTime=dayjs().format("dddd, MMMM D")
 var currentHour=dayjs().hour()
 var saveBtn=$(".saveBtn")
 
+// added code to display current day and time to header
 console.log(currentDayTime)
 currentDayEL.text(currentDayTime)
 console.log(currentHour)
 
+  // added for loop for time blocks to display classes for past, present, and future.
   for (let i = 9; i < 18; i++) {
     var timeBlock=$("#hour-"+i)
     var event=localStorage.getItem("hour-"+i)
@@ -29,6 +29,7 @@ console.log(currentHour)
     }
   }
 
+  // created function for savebtn to save information to local storage
   function saveEvent(event){
   var currentButton=$(event.target)
   var textArea=currentButton.siblings("textarea")
@@ -46,16 +47,8 @@ console.log(currentHour)
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
